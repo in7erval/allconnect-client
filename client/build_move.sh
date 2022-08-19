d3d55e1a683b4290b4a31fda367d5f99ed355d97
Dmitry Yudakov <yudakov.d@mail.ru>
2022-08-20 00:15:41 +0300

    Delete build_move.sh

--- client/build_move.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/sh
-
-# shellcheck disable=SC2028
-echo "export const API_URL = \"http://62.113.105.82:3001\";" > src/config.js
-npm run build
-scp -r build root@62.113.105.82:/root/allconnect/client/
-echo "export const API_URL = \"\";" > src/config.js
