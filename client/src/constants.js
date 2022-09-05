df77abb8c46fc74cca043c46b82761e03da36a58
Dmitry Yudakov <yudakov.d@mail.ru>
2022-09-05 20:45:44 +0300

    fixed

--- client/src/constants.js
+++
@@ -1,3 +0,0 @@
-export const TOKEN = 'token';
-export const USER_KEY = "user";
-export const SERVER_URI = process.env.REACT_APP_API_URL;
--- client/src/utils/storage.js
+++
@@ -1,9 +0,0 @@
-const storage = {
-	get: (key) =>
-		window.localStorage.getItem(key)
-			? JSON.parse(window.localStorage.getItem(key))
-			: undefined,
-	set: (key, value) => window.localStorage.setItem(key, JSON.stringify(value))
-}
-
-export default storage
